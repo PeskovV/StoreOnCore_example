0b4c39f6b6777b45844475573369bf37ce48fedb
Владислав <alladusheck@gmail.com>
2019-11-15 17:19:56 +0300

    Initial commit

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Interfaces;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+   public class ProductController : Controller
+   {
+      private readonly IProductRepository _repo;
+      public int PageSize = 4;
+
+      public ProductController(IProductRepository repo)
+      {
+         _repo = repo;
+      }
+
+      public ViewResult List(int productPage = 1)
+         => View(new ProductsListViewModel {
+            Products = _repo.Products
+               .OrderBy(p => p.ProductID)
+               .Skip((productPage - 1) * PageSize)
+               .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+               CurrentPage = productPage,
+               ItemsPerPage = PageSize,
+               TotalItems = _repo.Products.Count()
+            }
+         });
+   }
+}
---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using SportsStore.Models.Data;
+using SportsStore.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+   public class EFProductRepository : IProductRepository
+   {
+      private readonly ApplicationDbContext _context;
+
+      public EFProductRepository(ApplicationDbContext context)
+      {
+         _context = context;
+      }
+
+      public IQueryable<Product> Products => _context.Products;
+   }
+}
